13dfac27f5ea40131cec7513ed2990634a1656e5
vadim <vadimdrigula@gmail.com>
2024-11-02 16:33:16 +0200

    finish slider2

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ addEventListener('click',function () {
     const menu = document.querySelector('.nav');
     menu.classList.toggle('open');
 });
+document.querySelectorAll('.section5-list-item__img').forEach(img => {
+    img.addEventListener('click', function () {
+        const text = this.previousElementSibling.querySelector('.section5-list-item-texts__text');
+        text.classList.toggle('open');
+        this.classList.toggle('minus');
+    });
+});
+
